2137c5cee1cf6c1d497df9475b2655d9baf8c775
ElguetaM <vivianaandrea09@hotmail.com>
2024-03-27 19:13:03 -0300

    Hecha la funcion de redireccionar

--- index.js
+++ index.js
@@ -10,21 +10,24 @@ const __dirname = import.meta.dirname;
 
 app.use(express.static("assets"));
 
-app.get("/", async (req, res) => {
-  res.sendFile(path.join(__dirname, "/index.html"));
-  const id = uuidv4().slice(30);
-  const { image } = req.query;
-  const imagen = await Jimp.read(image);
-  await imagen
-    .resize(350, Jimp.AUTO)
-    .greyscale()
-    .writeAsync(`assets/img/${id}.jpg`);
-  const newImage = fs.readFileSync(`${id}.jpg`);
-  res.send(newImage);
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./index.html"));
 });
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "view/404.html"));
+app.get("/load", async (req, res) => {
+  try {
+    const id = uuidv4().slice(30);
+    const { image } = req.query;
+    const imagen = await Jimp.read(image);
+    const newImage = `${id}.jpg`;
+    await imagen
+      .resize(350, Jimp.AUTO)
+      .greyscale()
+      .writeAsync(`assets/img/${newImage}`);
+    res.sendFile(path.join(__dirname, `assets/img/${newImage}`));
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 app.listen(PORT, () =>
